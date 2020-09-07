941d5924891e7514d99f8d23cc24a91401ef4029
singhronak06 <70131440+singhronak06@users.noreply.github.com>
2020-09-07 13:54:50 +0530

    Headers

---
+++ Lab1/header.h
@@ -0,0 +1,11 @@
+extern void sort(int size, int x[]);
+
+extern float mean(int size, int x[]);
+
+extern float median(int size, int x[]); 
+ 
+extern float mode(int size, int x[]);
+
+extern int max(int size, int x[]);
+
+extern int min(int size, int x[]);
